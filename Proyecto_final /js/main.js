b3f3adfb89b3221346864deda49adf56297c6997
ordep96 <pedro.pmo55@gmail.com>
2016-07-10 21:12:37 -0400

    modificacion de codigo js

--- Proyecto_final /js/main.js
+++ Proyecto_final /js/main.js
@@ -1,11 +1,15 @@
 $('#comprobar').click(function(){
 	var nombre = $('#nombre').val();
 	var apellido = $('#apellido').val();
+	var empresa = $('#empresa').val();
+    	
     	var validacion = /^[a-zA-Z]*$/;
     	if( !validacion.test(nombre) || nombre === "") {
-    		alert("Campo nombre Incorrecto");
+    			alertify.alert("Opps","CAMPO NOMBRE INCORRECTO");
         }else if(!validacion.test(apellido) || apellido === " " ){
-        	alert("Campo apellido Incorrecto");
+        	alertify.alert("Opps","CAMPO APELLIDO INCORRECTO");
+        }else if(empresa === ""){
+        	alertify.alert("Opps","CAMPO EMPRESA NO PUEDE ESTAR VACIO");
         }
 });
 
