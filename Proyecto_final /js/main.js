fcc18094cfc6ba4d44014966592c19cc84be3d96
ordep96 <pedro.pmo55@gmail.com>
2016-07-11 15:35:20 -0400

    Modificacion de main.js

--- Proyecto_final /js/main.js
+++ Proyecto_final /js/main.js
@@ -4,10 +4,14 @@ $('#comprobar').click(function(){
 	var empresa = $('#empresa').val();
     	
     	var validacion = /^[a-zA-Z]*$/;
-    	if( !validacion.test(nombre) || nombre === "") {
+    	if( !validacion.test(nombre)) {
     			alertify.alert("Opps","CAMPO NOMBRE INCORRECTO");
-        }else if(!validacion.test(apellido) || apellido === " " ){
+        }else if(nombre === ""){
+                alertify.alert("Opps","CAMPO NOMBRE NO PUEDE ESTAR VACIO");
+        }else if(!validacion.test(apellido)){
         	alertify.alert("Opps","CAMPO APELLIDO INCORRECTO");
+        }else if(apellido === ""){
+            alertify.alert("Opps","CAMPO APELLIDO NO PUEDE ESTAR VACIO");
         }else if(empresa === ""){
         	alertify.alert("Opps","CAMPO EMPRESA NO PUEDE ESTAR VACIO");
         }
